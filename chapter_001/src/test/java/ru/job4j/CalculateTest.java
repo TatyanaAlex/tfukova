ae2fb68a0bd5fa331bb70152cff60c3251054ec8
TatyanaAlex <tatyanafukova@gmail.com>
2017-08-31 18:58:00 +0300

    JUnit task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Tatyana Fukova (mailto:tatyanafukova@gmail.com)
+* @version 1
+* @since 31.08.2017
+*/
+
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+
+public void whenTakeNameThenTreeEchoPlusName() {
+  String input = "Tatyana Fukova";
+ String expect = "Echo, echo, echo : Tatyana Fukova";
+ 
+ //Создание нового объекта.
+Calculate calc = new Calculate();
+
+//Выполнение метода echo с параметром input и запись ее в переменную result.
+String result = calc.echo(input); 
+
+//сравнение двух значений result и expect
+assertThat(result, is(expect));
+}
+
+}
