adf881478feee035e95d5dfadd2eeb61fe4b811e
TatyanaAlex <tatyanafukova@gmail.com>
2017-08-31 14:38:34 +0300

    JavaDoc-task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j;
+/**
+ *Class Calculate выполнение задания 1.3. JavaDoc. Code convention
+ *@author tfukova
+ *@version 1
+ *@since 31.08.2017
+ */
 public class Calculate{
+	/**
+	* Метод - вывод строки в консоль
+	* @param args -args. входящий параметр
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Tatyana Fukova (mailto:tatyanafukova@gmail.com)
+* @version 1
+* @since 31.08.2017
+*/
+package ru.job4j;
