ab07296f6a1101d2cfcbc672990b45d98f8cf2ae
TatyanaAlex <tatyanafukova@gmail.com>
2017-10-12 01:59:45 +0300

    tracker

---
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,81 @@
+package ru.job4j.tracker;
+
+public class Tracker {
+
+    private Item[] items = new Item[100];
+    private int position = 0;
+
+    public Item add(Item item){
+        this.items[position++] = item;
+        return item;
+    }
+
+    public void update(Item itemToUpdate){
+
+        for (int i = 0; i < items.length; i++) {
+            if(items[i]!=null && items[i].getId().equals(itemToUpdate.getId())){
+                items[i] = itemToUpdate;
+                break;
+            }
+        }
+    }
+    public void delete(Item itemToDelete){
+        for (int i = 0; i < items.length ; i++) {
+            if(items[i]!=null && items[i].getId().equals(itemToDelete.getId())){
+                items[i] = null;
+                break;
+            }
+
+        }
+
+    }
+    public Item[] findAll(){
+        Item[] itemsToCopy = new Item[this.position];
+        int count = 0;
+        for (int i = 0; i <items.length ; i++) {
+            if(items[i]!= null){
+                itemsToCopy[count]=items[i];
+                count++;
+            }
+
+        }
+        Item[] finalArray = new Item[count];
+        System.arraycopy(itemsToCopy,0, finalArray,0, count);
+        return finalArray;
+    }
+
+    public Item[] findByName(String key){
+        Item[] temp = new Item[this.position];
+        int count =0;
+        for (int i = 0; i< items.length ; i++) {
+            if(items[i]!=null && items[i].getName().equals(key)){
+                temp[count] = items[i];
+                count++;
+            }
+
+        }
+
+        Item[] resultArray = new Item[count];
+        System.arraycopy(temp,0, resultArray,0, count);
+        return resultArray;
+
+    }
+
+    public Item findById(String id){
+        Item result = null;
+        for(Item item : items){
+            if (item != null && item.getId().equals(id) ){
+                result = item;
+                break;
+            }
+
+        }return result;
+
+    }
+
+}
+
+
+
+
+
---
+++ chapter_001/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1","testDescription",123L);
+        tracker.add(item);
+        assertThat(tracker.findAll()[0], is(item));
+    }
+    @Test
+    public void whenUpdateNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1","testDescription",123L);
+        previous.setId("This is ID number 1");
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(previous);
+        // Создаем новую заявку.
+        Item next = new Item("test2","testDescription2",1234L);
+        // Проставляем старый id из previous, который был сгенерирован выше.
+        next.setId(previous.getId());
+        // Обновляем заявку в трекере.
+        tracker.update(next);
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+    }
+    @Test
+    public void whenDeleteItemThenArrayWithoutIt() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1","testDescription",123L);
+        item.setId("This is ID number 1");
+        tracker.add(item);
+        tracker.delete(item);
+        Item temp = null;
+        assertThat(tracker.findById("This is ID number 1"), is(temp));
+    }
+
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1","testDescription",123L);
+        item.setId("This is ID number 1");
+        tracker.add(item);
+        Item[] arrayToFind = {item};
+        assertThat(tracker.findByName("test1"), is(arrayToFind));
+    }
+    
+
+}
