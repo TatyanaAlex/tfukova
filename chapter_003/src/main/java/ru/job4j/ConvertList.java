a68adac7b8baddf8b75714479cda3994f9938349
TatyanaAlex <tatyanafukova@gmail.com>
2017-12-18 01:18:16 +0300

    convert array to list and back

---
+++ chapter_003/src/main/java/ru/job4j/ConvertList.java
@@ -0,0 +1,57 @@
+package ru.job4j;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Class ConvertList.
+ * @author Tatyana Fukova (mailto:tatyanafukova@gmail.com)
+ * @version $I$
+ * @since 17.12
+ */
+public class ConvertList {
+
+    /**
+     * Method to convert array to list.
+     * @param array double array.
+     * @return list.
+     */
+    public List<Integer> toList (int[][] array) {
+
+        List<Integer> list = new ArrayList<>();
+        for (int[] x  : array ) {
+            for (int y : x){
+                list.add(Integer.valueOf(y));
+            }
+        }
+    return list;
+    }
+    
+    /**
+     * Method to convert list to array.
+     * @param list given collection.
+     * @param rows number of rows in the array
+     * @return array.
+     */
+    public int[][] toArray (List<Integer> list, int rows) {
+
+        int rowlength = list.size()/rows;
+        rowlength += list.size()%rows;
+        Iterator iterator = list.iterator();
+        int[][] array = new int[rows][rowlength];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rowlength ; j++) {
+                if (iterator.hasNext()) {
+                    array[i][j] = Integer.parseInt(iterator.next().toString());
+                }else{
+                    array[i][j] = 0;
+                }
+
+            }
+
+        }
+
+         return array;
+    }
+}
