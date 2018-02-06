6f7f0323e7750235f14565ae58893cc3ea005707
TatyanaAlex <tatyanafukova@gmail.com>
2018-02-06 03:45:55 +0300

    Lists compare

---
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,47 @@
+package ru.job4j.comparator;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Class ListCompare.
+ * @author Tatyana Fukova (mailto:tatyanafukova@gmail.com)
+ * @version $I$
+ * @since 18.01
+ */
+public class ListCompare implements Comparator<List<Integer>> {
+    /**
+     * Method to compare two lists.
+     * @param left, right  two given lists.
+     * @return result.
+     */
+    @Override
+    public int compare(List<Integer> left, List<Integer> right) {
+
+        Collections.sort(left);
+        Collections.sort(right);
+
+        int maxLenght = 0;
+        if (left.size() <= right.size()) {
+            maxLenght = left.size();
+        } else {
+            maxLenght = right.size();
+        }
+
+        for (int i = 0; i < maxLenght; i++) {
+            if (left.get(i).compareTo(right.get(i)) == 0) {
+                continue;
+            } else {
+                return left.get(i).compareTo(right.get(i));
+            }
+        }
+        int diff = left.size() - right.size();
+        if (diff < 0) {
+            return -1;
+        } else if (diff > 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/comparator/ListCompareTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.comparator;
+
+import org.junit.Test;
+import java.util.Arrays;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class ListCompareTest.
+ * @author Tatyana Fukova (mailto:tatyanafukova@gmail.com)
+ * @version $I$
+ * @since 18.01
+ */
+public class ListCompareTest {
+    /**
+     * Test method.
+     */
+    @Test
+    public void whenLeftAndRightEqualsThenZero() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(1, 2, 3)
+        );
+        assertThat(rst, is(0));
+    }
+
+    /**
+     * Test method.
+     */
+    @Test
+    public void whenLeftLessRightThenMinus() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                Arrays.asList(1),
+                Arrays.asList(1, 2, 3)
+        );
+        assertThat(rst, is(-1));
+    }
+
+    /**
+     * Test method.
+     */
+    @Test
+    public void whenLeftGreatRightThenPlus() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                Arrays.asList(1, 2),
+                Arrays.asList(1, 1)
+        );
+        assertThat(rst, is(1));
+    }
+}
