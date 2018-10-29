9e3b6f9b48bff017c1d0c644319edb840c7751fc
TatyanaAlex <tatyanafukova@gmail.com>
2018-10-29 10:16:46 +0000

    PingPong

---
+++ multithreading/src/main/java/ru/job4j/threads/RectangleMove.java
@@ -0,0 +1,44 @@
+package ru.job4j.threads;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ * Class RectangleMove.
+ */
+public class RectangleMove implements Runnable {
+
+    private final Rectangle rect;
+
+    /**
+     * Constructor.
+     *
+     * @param rect rectangle.
+     */
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        boolean moveStraight = true;
+        while (true) {
+            if (this.rect.getX() == 295) {
+                moveStraight = false;
+            }
+            if (this.rect.getX() == 0) {
+                moveStraight = true;
+            }
+            if (moveStraight) {
+                this.rect.setX(this.rect.getX() + 1);
+            } else {
+                this.rect.setX(this.rect.getX() - 1);
+            }
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
