2af65be5bbd453bbfab5ff6e5b1239f6fe4f0651
TatyanaAlex <tatyanafukova@gmail.com>
2018-11-25 22:47:23 +0000

    JMM

---
+++ multithreading/src/main/java/ru/job4j/jmm/ThreadsProblem.java
@@ -0,0 +1,64 @@
+package ru.job4j.jmm;
+
+/**
+ * Class ThreadsProblem.
+ */
+public class ThreadsProblem {
+
+    public static void main(String[] args) {
+
+        Counter counter = new Counter();
+
+        for(int i=0; i < 200; i++) {
+            CounterInThread ct = new CounterInThread(counter);
+            ct.start();
+
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter: " + counter.getCounter());
+    }
+
+
+
+}
+
+/**
+ * Class Counter shared object.
+ */
+class Counter {
+
+    private long counter = 0L;
+
+    public long getCounter() {
+        return counter;
+    }
+
+    public void increseCounter() {
+        counter++;
+    }
+}
+
+/**
+ * Class CounterInThread.
+ * Thread increases counter for 1.
+ */
+class CounterInThread extends Thread {
+
+    private Counter counter;
+
+    public CounterInThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0; i<1000; i++) {
+            counter.increseCounter();
+        }
+    }
+}
