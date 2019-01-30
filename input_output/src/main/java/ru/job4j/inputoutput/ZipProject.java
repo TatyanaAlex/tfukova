5b12519e1a4883ae782207e1e851a5c5d1fbdfc3
TatyanaAlex <tatyanafukova@gmail.com>
2019-01-30 00:31:36 +0000

    Zip project

---
+++ input_output/src/main/java/ru/job4j/inputoutput/ZipProject.java
@@ -0,0 +1,57 @@
+package ru.job4j.inputoutput;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Class ZipProject.
+ */
+public class ZipProject {
+
+    public static void main(String[] args) {
+        String srcDir = "/Users/tatyanafukova/projects";
+        String zip = "/Users/tatyanafukova/project.zip";
+        String ext = "xml";
+        List<String> exts = new ArrayList<>();
+        exts.add(ext);
+        new ZipProject().zip(exts, srcDir, zip);
+
+    }
+
+    /**
+     * method to zip files with an appropriate extension.
+     *
+     * @param srcDir source directory.
+     * @param exts   files´ extension.
+     * @param zip zipped directory.
+     */
+    public void zip(List<String> exts, String srcDir, String zip) {
+        byte[] buffer = new byte[1024];
+        Search search = new Search();
+        try {
+            ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zip));
+            List<File> files = search.files(srcDir, exts);
+            for (int i = 0; i < files.size(); i++) {
+                File tempfile = files.get(i);
+                FileInputStream fis = new FileInputStream(tempfile);
+                zos.putNextEntry(new ZipEntry(tempfile.getAbsolutePath()));
+                int length;
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length);
+                }
+                zos.closeEntry();
+                fis.close();
+            }
+            zos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
