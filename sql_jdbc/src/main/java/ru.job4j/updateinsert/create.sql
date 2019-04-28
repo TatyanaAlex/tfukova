aa8cdbae07274a30c3cfdb13a8a2e4e2d6a449f2
TatyanaAlex <tatyanafukova@gmail.com>
2019-04-28 21:17:11 +0100

    modified script

--- sql_jdbc/src/main/java/ru.job4j/updateinsert/create.sql
+++ sql_jdbc/src/main/java/ru.job4j/updateinsert/create.sql
@@ -1,37 +1,63 @@
---create new database
-create database applications_system;
+CREATE DATABASE system_of_items;
 
-
---start using database applications_system
- \c applications_system
-
---create new table of users with the fields: name, role, rights
-create table users(
-name character varying (50),
-role character varying (50),
-rights character varying (50),
-	primary key(name)
+CREATE TABLE roles(
+	id serial primary key,
+	role_name varchar(100)
 );
 
--- Create new table "aplications", with fields: name, comments, status,
--- categories, responsibility, role, permission. Primary key "name".
-CREATE TABLE aplications (
-	name character varying (50),
-	comments character varying (250),
-	status boolean,
-	categories character varying (50),
-	responsibility character varying (50)
-	references users(name),
-	role character varying (50),
-	permission boolean,
-	PRIMARY KEY(name)
+CREATE TABLE users(
+	id serial primary key,
+	user_name varchar(100),
+	role_id int references roles(id)
 );
 
--- Add new data into table "users".
-INSERT INTO users(name, role, rights)
-VALUES ('maria', 'doctor', 'treatment');
+CREATE TABLE rules(
+	id serial primary key,
+	rule_name varchar(100)
+);
 
--- Add new data into table "aplications".
-INSERT INTO aplications(name, comments, status, categories, responsibility, role,
-permission)
-VALUES ('aplic 1', 'first application', TRUE, 'sql', 'maria', 'doctor', TRUE);
+CREATE TABLE states(
+	id serial primary key,
+	state_name varchar(100)
+);
+
+CREATE TABLE categories(
+	id serial primary key,
+	cat_name varchar(100)
+);
+
+CREATE TABLE items(
+	id serial primary key,
+	item_name varchar(100),
+	user_id int references users(id),
+	category_id int references categories(id),
+	state_id int references states(id)
+);
+
+CREATE TABLE commentaries(
+	id serial primary key,
+	comm_text varchar(3000)
+);
+
+CREATE TABLE attachments(
+	id serial primary key,
+	attach_path varchar(2000)
+);
+
+CREATE TABLE role_rules(
+	id serial primary key,
+	id_role int references roles(id),
+    id_rule int references rules(id)
+);
+
+CREATE TABLE item_comments(
+	id serial primary key,
+	id_item int references items(id),
+    id_comment int references commentaries(id)
+);
+
+CREATE TABLE item_attachments(
+	id serial primary key,
+	id_item int references items(id),
+    id_attachment int references attachments(id)
+);
